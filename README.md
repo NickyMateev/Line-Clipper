4e1330df0d511e6b19f851023567b66fa8e8cb5a
NickyMateev <bgdunkers@gmail.com>
2020-04-23 09:48:34 +0300

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# Line-Clipper
-Effortlessly copy line numbers with their respective file path directly from GitHub.
+# <img src="images/logo128.png" align="center"> Line Clipper
+
+**LineClipper** is a Chrome Extension which aims to optimize a software developer's
+flow between the browser and the IDE when executing file retrieval.
+This particular extension works on the GitHub source control platform.
+
+## Motivation
+
+A lot of IDEs provide quick find capabilities where you can specify a file path
+and the IDE would open that file for you. Furthermore, you can also specify a line number
+of said file at which you'd like to go to. Example:
+
+`api/controllers/base_controller.go:73`
+
+The following would take us to the `base_controller.go` file on line 73 in the *api/controllers* directory.
+
+When working with GitHub and especially when making code reviews it's common for a developer
+to jump from the browser to the IDE and vice versa. Often the developer would look at a file
+and would want to quickly find and open that file at a particular point (line). To achieve this
+today the individual would have to go over the following steps:
+
+1. Copy file path by selecting the copy button at the top of the file (in some perspectives there isn't even any copy button and you have to select and copy the path manually)
+2. Move his attention back to the critical piece of code and remember the line number of interest
+3. Go to the IDE and trigger quick find
+4. Paste the file path that was copied
+5. Remember to also append and write the line number of interest
+6. Execute search
+
+## Solution
+
+This extension tries to optimize this flow by providing simplistic functionality which copies both the
+file path and also appends the corresponding line number to it. All an individual has to do is click
+on the line number of interest. In this way we optimize the above flow not only in steps, but also in
+mental effort. You can now focus on what you want to do and not worry about petty details which break flow:
+
+1. Copy line number by clicking the number of the line in the file of interest
+2. Go to the IDE and trigger quick find
+3. Paste the file path that was copied (it includes the appended line number)
+4. Execute search
+
+## Features:
+
+The extension focuses on doing one thing and do it well - copy file paths with their respective line numbers to your clipboard when clicking on specific line numbers while viewing any
+type of file with line numbering on GitHub.
+
+#### Simplistic - just click on the desired line number:
+
+<img src="promotional/line_focus_promotional.png" align="center">
+
+#### While doing code review:
+
+<img src="promotional/review_focus_promotional.png" align="center">
+
+#### During conversation:
+
+<img src="promotional/conversation_focus_promotional.png" align="center">
+
+#### When going through repository files:
+
+<img src="promotional/file_focus_promotional.png" align="center">
+
+
+### FAQ
+
+Q: *Does this work only when traversing a GitHub project's file tree and looking at different files?*
+
+A: No, it works for **every page** on GitHub which has a file with lines in it. You can copy lines from
+the GitHub Pull request diff perspective when you're looking at the file diffs or at the main page of a
+pull request where sometimes we see some code snippets which have been commented. You can also copy line numbers
+when you're going through old commits and looking around at the files.
+
+Q: *Does it work for all GitHub installations or just the public *https://github.com* installation?*
+
+A: The extension works on all kinds of GitHub installations public/private as long as the domain contains
+the word *github* in it.
---
+++ traverser.js
@@ -0,0 +1,124 @@
+function constructClipBtn(displayTag) {
+  var clipBtn = document.createElement("clipboard-copy")
+  clipBtn .classList = "js-clipboard-copy zeroclipboard-link text-gray link-hover-blue"
+  clipBtn .setAttribute("aria-label", "Copy")
+  clipBtn .setAttribute("data-copy-feedback", "Copied!")
+  clipBtn .setAttribute("tabindex", "0")
+  clipBtn .setAttribute("role", "button")
+  
+  var checkSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+  checkSvg.classList = "octicon octicon-check js-clipboard-check-icon mx-1 d-inline-block text-green d-none"
+  checkSvg.setAttributeNS("http://www.w3.org/2000/svg", "viewBox", "0 0 12 16")
+  checkSvg.setAttribute("width", "12px")
+  checkSvg.setAttribute("height", "16px")
+  checkSvg.setAttribute("version", "1.1")
+  checkSvg.setAttribute("aria-hidden", "true")
+
+  var svgPath = document.createElementNS("http://www.w3.org/2000/svg", "path")
+  svgPath.setAttribute("fill-rule", "evenodd")
+  svgPath.setAttribute("d", "M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z")
+
+  var displayPath = document.createElement("path")
+  displayPath.setAttribute("fill-rule", "evenodd")
+  displayPath.setAttribute("d", "M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z")
+
+  displayTag.classList = "d-inline-block mx-1 js-clipboard-clippy-icon"
+  displayTag.appendChild(displayPath)
+  checkSvg.appendChild(svgPath)
+
+  clipBtn.appendChild(displayTag)
+  clipBtn.appendChild(checkSvg)
+
+  return clipBtn
+}
+
+function fileTraverser(file) {
+    return function() {
+      traverseFile(file, retrieveFilePath(file))
+    }
+}
+
+function attachDOMElementListener(element, func, observerOptions) {
+  new MutationObserver(func).observe(element, observerOptions)
+}
+
+async function traverseFile(file, filePath) {
+  lines = file.querySelectorAll('[data-line-number]')
+
+  for (line of lines) {
+    lineNumber = line.getAttribute("data-line-number")
+    if (lineNumber == "...") {
+      continue
+    }
+
+    path = filePath + ":" + lineNumber
+
+    textTag = document.createElement("text")
+    textTag.innerHTML = lineNumber
+    textTag.style = "color: rgba(27, 31, 35, 0.3)"
+
+
+    lineClipBtn = constructClipBtn(textTag)
+    lineClipBtn.setAttribute("value", path)
+
+    line.appendChild(lineClipBtn)
+
+    line.removeAttribute("data-line-number")
+  }
+
+  expandableLines = file.getElementsByClassName("js-expandable-line")
+  for (var i = 0; i < expandableLines.length; i++) {
+    attachDOMElementListener(expandableLines[i].parentNode, fileTraverser(file), {childList: true})
+  }
+}
+
+function retrieveFilePath(file) {
+  return file.getElementsByTagName("a")[0].title
+}
+
+function traverseSingleFilePage() {
+  finalPathTag = document.getElementsByClassName("final-path")
+  if (finalPathTag.length == 0) {
+    return
+  }
+
+  fileName = finalPathTag[0].textContent
+
+  var filePath = ""
+  pathTokens = document.getElementsByClassName("js-path-segment")
+  for (var i = 1; i < pathTokens.length; i++) {
+    filePath += pathTokens[i].textContent + "/"
+  }
+
+  filePath += fileName
+  traverseFile(document, filePath)
+}
+
+function traverseMultiFilePage() {
+  files = document.getElementsByClassName("file")
+
+  for (var i = 0; i < files.length; i++) {
+    attachDOMElementListener(files[i], fileTraverser(files[i]), {attributes: true})
+    traverseFile(files[i], retrieveFilePath(files[i]))
+  }
+
+  if (files.length > 0) { // Sometimes there are files hidden behind "Load more" buttons - we need to listen for changes on container DOM parent too detect when new files are "revealed"
+    attachDOMElementListener(files[0].parentNode.parentNode, traverseMultiFilePage, {childList: true, subtree: true})
+  }
+}
+
+function processFile() {
+  var path = document.querySelectorAll('[name=analytics-location]')[0].getAttribute("content")
+  var hasMultipleFiles = path.includes("pull_requests") || path.includes("commit")
+
+  var traverseFileFunc = traverseSingleFilePage
+  if (hasMultipleFiles) {
+    traverseFileFunc = traverseMultiFilePage
+  }
+  traverseFileFunc()
+}
+
+
+pageTitle = document.getElementsByTagName("title")[0]
+attachDOMElementListener(pageTitle, processFile, {childList: true})
+processFile()
